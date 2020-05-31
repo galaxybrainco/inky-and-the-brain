d050ada78494d90f7e4d1c5932a655e216091e41
phildini <phildini@phildini.net>
2020-05-31 05:33:26 +0000

    Deploying to gh-pages from  @ c1d7e19e1a1130e62371f387b59430fdff8ab414 🚀

---
+++ myStory.js
@@ -0,0 +1,2 @@
+var storyContent = 
+﻿{"inkVersion":19,"root":[["^Hello! This repo contains the tools needed to setup an ink => github pages pipeline. Check out github.com/galaxybrainco/inky-and-the-brain for more.","\n",["done",{"#n":"g-0"}],null],"done",null],"listDefs":{}};
